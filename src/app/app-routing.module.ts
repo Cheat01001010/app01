78579894fe3a6e776e7b397fd2b5efb8d723c40a
Cheat01001010 <joaopedrosilva802@gmail.com>
2022-03-15 16:53:08 -0300

    01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,38 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./page/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./page/users/users.module').then( m => m.UsersPageModule)
+  },
+  {
+    path: 'docs',
+    loadChildren: () => import('./page/docs/docs.module').then( m => m.DocsPageModule)
+  },
+  {
+    path: 'contacts',
+    loadChildren: () => import('./page/contacts/contacts.module').then( m => m.ContactsPageModule)
+  },
+  {
+    path: 'abouts',
+    loadChildren: () => import('./page/abouts/abouts.module').then( m => m.AboutsPageModule)
+  },
+
+  /**
+   * Esta deve ser SEMPRE a última rota desta lista.
+   * Se criar novas páginas, edite este arquivo para satisfazer a regra acima.
+   */
+
+  {
+    path: '**',
+    loadChildren: () => import('./page/e404/e404.module').then( m => m.E404PageModule)
+  },
+
 ];
 
 @NgModule({
